ed44e7b5d8ed2daa6f5fdab7bd40d059be1a0976
johnnoG <95978094+johnnoG@users.noreply.github.com>
2022-03-08 14:44:16 +0200

    home assignment 1- rec

--- home assignment 1 - rec/home assignment 1 - rec/Q2.c
+++ home assignment 1 - rec/home assignment 1 - rec/Q2.c
@@ -1,52 +1,52 @@
-////
-////  Q2.c
-////  home assignment 1 - rec
-////
-////  Created by Yonatan Glanzman on 07/03/2022.
-////
 //
-//#include "Q2.h"
-//#include <stdio.h>
-//#define N 2
-//int recurse(int mat[][N],int rows,int cols, int row, int col);
-//int path_exists(int mat[][N],int rows,int cols);
+//  Q2.c
+//  home assignment 1 - rec
 //
-//void main(){
-//    int mat[N][N];
-//    printf("enter %d 1's or 0's to the matrix\n",N*N);
-//    
-//    for (int i = 0; i < N; i++) {
-//        for (int j = 0; j < N; j++) {
-//            printf("enter value for row,col (%d,%d): ",i,j);
-//            scanf("%d",&mat[i][j]);
-//            printf("\n");
-//        }
-//    }
-//    if (path_exists(mat, N, N) == 1){
-//        printf("good\n");
-//    }
-//    else printf("not good\n");
-//}
+//  Created by Yonatan Glanzman on 07/03/2022.
 //
-//int path_exists(int mat[][N],int rows,int cols){
-//    return recurse( mat, rows, cols,  0, 0);
-//}
-//
-//int recurse(int mat[][N],int rows,int cols, int row, int col){
-//    if(row >= rows || col >= cols){
-//        return 0;
-//    }
-//
-//    if(mat[row][col]!=1){
-//        return 0;
-//    }
-//    
-//    if (row == rows - 1 && col == cols -1){
-//        return 1;
-//    }
-//    
-//    return
-//        recurse(mat, rows, cols,  row, col+1) ||
-//        recurse(mat, rows, cols,  row+1, col+1) ||
-//        recurse(mat, rows, cols,  row+1, col);
-//}
+
+#include "Q2.h"
+#include <stdio.h>
+#define N 2
+int recurse(int mat[][N],int rows,int cols, int row, int col);
+int path_exists(int mat[][N],int rows,int cols);
+
+void main(){
+    int mat[N][N];
+    printf("enter %d 1's or 0's to the matrix\n",N*N);
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("enter value for row,col (%d,%d): ",i,j);
+            scanf("%d",&mat[i][j]);
+            printf("\n");
+        }
+    }
+    if (path_exists(mat, N, N) == 1){
+        printf("good\n");
+    }
+    else printf("not good\n");
+}
+
+int path_exists(int mat[][N],int rows,int cols){
+    return recurse( mat, rows, cols,  0, 0);
+}
+
+int recurse(int mat[][N],int rows,int cols, int row, int col){
+    if(row >= rows || col >= cols){
+        return 0;
+    }
+
+    if(mat[row][col]!=1){
+        return 0;
+    }
+    
+    if (row == rows - 1 && col == cols -1){
+        return 1;
+    }
+    
+    return
+        recurse(mat, rows, cols,  row, col+1) ||
+        recurse(mat, rows, cols,  row+1, col+1) ||
+        recurse(mat, rows, cols,  row+1, col);
+}
--- home assignment 1 - rec/home assignment 1 - rec/Q3.c
+++ home assignment 1 - rec/home assignment 1 - rec/Q3.c
@@ -1,47 +1,47 @@
-////
-////  Q3.c
-////  home assignment 1 - rec
-////
-////  Created by Yonatan Glanzman on 07/03/2022.
-////
 //
-//#include "Q3.h"
-//#include <stdio.h>
-//#include <string.h>
-//#include <stdlib.h>
-//int strcomp(char* str1, char* str2);
+//  Q3.c
+//  home assignment 1 - rec
 //
-//void main(){
-//    char str1[30];
-//    char str2[30];
-//    printf("please  enter TWO strings:\n");
-//    gets(str1);
-//    gets(str2);
+//  Created by Yonatan Glanzman on 07/03/2022.
 //
-//    int x = strcomp(str1, str2);
-//
-//    if (x > 0) {
-//        printf("first string's bigger\n");
-//    }
-//    if (x < 0) {
-//        printf("second string's bigger\n");
-//    }
-//    if (x == 0) {
-//        printf("strings are equal\n");
-//    }
-//}
-//
-//int strcomp(char* str1, char* str2){
-//
-//
-//    if (!*str1 || !*str2) {
-//        return 0;
-//       }
-//    if (*str1 > *str2) {
-//       return 1;
-//    }
-//    if (*str1 < *str2) {
-//        return -1;
-//    }
-//    else return strcomp(str1 + 1, str2 + 1);
-//}
+
+#include "Q3.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int strcomp(char* str1, char* str2);
+
+void main(){
+    char str1[30];
+    char str2[30];
+    printf("please  enter TWO strings:\n");
+    gets(str1);
+    gets(str2);
+
+    int x = strcomp(str1, str2);
+
+    if (x > 0) {
+        printf("first string's bigger\n");
+    }
+    if (x < 0) {
+        printf("second string's bigger\n");
+    }
+    if (x == 0) {
+        printf("strings are equal\n");
+    }
+}
+
+int strcomp(char* str1, char* str2){
+
+
+    if (!*str1 || !*str2) {
+        return 0;
+       }
+    if (*str1 > *str2) {
+       return 1;
+    }
+    if (*str1 < *str2) {
+        return -1;
+    }
+    else return strcomp(str1 + 1, str2 + 1);
+}
